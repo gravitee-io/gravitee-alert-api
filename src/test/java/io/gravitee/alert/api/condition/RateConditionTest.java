9d03a30de1d39f6aac3fd062f635af50fbbbc752
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2021-03-09 19:17:27 +0100

    feat(rate-condition): add minimum sample size
    Fixes gravitee-io/gravitee-alert-engine#181

--- src/test/java/io/gravitee/alert/api/condition/RateConditionTest.java
+++ src/test/java/io/gravitee/alert/api/condition/RateConditionTest.java
@@ -37,12 +37,47 @@ public class RateConditionTest {
      * This rule is testing the following use-case:
      *
      * Calculate the percentage (rate) of events (which match the given filters) for which the "latency" value is
-     * greater or equals than 500 milliseconds during the last minute.
+     * greater or equals than 500 milliseconds during the last minute with a minimum sample size set to 20 samples.
      *
      * The condition is evaluated to true if the calculated rate is lower than 40%.
      */
     @Test
     public void shouldBuildRateCondition_lowerThan() {
+        RateCondition condition = RateCondition
+                .of(ThresholdCondition.greaterThanOrEquals("latency", 500d).build())
+                .duration(1, TimeUnit.MINUTES)
+                .lowerThan(40d)
+                .projection(Projections.property("api"))
+                .sampleSize(20)
+                .build();
+
+        // Check condition
+        Assert.assertNotNull(condition);
+        Assert.assertEquals(40d, condition.getThreshold(), 0);
+        Assert.assertEquals(RateCondition.Operator.LT, condition.getOperator());
+        Assert.assertEquals(1, condition.getDuration());
+        Assert.assertEquals(TimeUnit.MINUTES, condition.getTimeUnit());
+        Assert.assertEquals(60 * 1000, condition.getWindowTime());
+        Assert.assertEquals(20, condition.getSampleSize());
+
+        // Check comparison
+        Assert.assertNotNull(condition.getComparison());
+        Assert.assertEquals(ThresholdCondition.class, condition.getComparison().getClass());
+        Assert.assertEquals("latency", ((ThresholdCondition)condition.getComparison()).getProperty());
+        Assert.assertEquals(ThresholdCondition.Operator.GTE, ((ThresholdCondition)condition.getComparison()).getOperator());
+        Assert.assertEquals((Double) 500d, ((ThresholdCondition)condition.getComparison()).getThreshold());
+
+        // Check projection
+        Assert.assertNotNull(condition.getProjections());
+        Assert.assertEquals(1, condition.getProjections().size());
+        Assert.assertEquals("api", ((PropertyProjection) condition.getProjections().get(0)).getProperty());
+    }
+
+    /**
+     * This rule is testing that minimum sample size is set to 1 if not defined.
+     */
+    @Test
+    public void shouldBuildRateCondition_defaultSampleSize() {
         RateCondition condition = RateCondition
                 .of(ThresholdCondition.greaterThanOrEquals("latency", 500d).build())
                 .duration(1, TimeUnit.MINUTES)
@@ -57,17 +92,6 @@ public class RateConditionTest {
         Assert.assertEquals(1, condition.getDuration());
         Assert.assertEquals(TimeUnit.MINUTES, condition.getTimeUnit());
         Assert.assertEquals(60 * 1000, condition.getWindowTime());
-
-        // Check comparison
-        Assert.assertNotNull(condition.getComparison());
-        Assert.assertEquals(ThresholdCondition.class, condition.getComparison().getClass());
-        Assert.assertEquals("latency", ((ThresholdCondition)condition.getComparison()).getProperty());
-        Assert.assertEquals(ThresholdCondition.Operator.GTE, ((ThresholdCondition)condition.getComparison()).getOperator());
-        Assert.assertEquals((Double) 500d, ((ThresholdCondition)condition.getComparison()).getThreshold());
-
-        // Check projection
-        Assert.assertNotNull(condition.getProjections());
-        Assert.assertEquals(1, condition.getProjections().size());
-        Assert.assertEquals("api", ((PropertyProjection) condition.getProjections().get(0)).getProperty());
+        Assert.assertEquals(1, condition.getSampleSize());
     }
 }
